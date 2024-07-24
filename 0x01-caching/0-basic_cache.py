13a1fe3b6c6272732feba389994780901b8725de
asmaagoma <asmaagadallaah@gmail.com>
2024-07-24 21:42:11 +0300

    :"(

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+put and get
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    basic cache module
+    """
+
+    def put(self, key, item):
+        """
+        put func
+        """
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        get func
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
