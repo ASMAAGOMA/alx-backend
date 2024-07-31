e7217441341028c05bdcfacc27d998ed69ff393f
asmaagoma <asmaagadallaah@gmail.com>
2024-07-31 04:04:21 +0300

    :"(

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+app module
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    configure langs
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get locale method
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    index method
+    """
+    return render_template('./3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
